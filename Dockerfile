8d39a7d2cc2cd72791c2384d5f20394877decdf3
Rich <rwilson@us.ibm.com>
2024-02-06 21:09:19 -0700

    Feat: uploading

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM ubuntu:16.04
-RUN apt-get update && apt-get install -y python python-pip
-RUN pip install flask
+FROM ubuntu
+RUN apt-get clean
+RUN apt-get update && apt-get install -y python-is-python3 python3-pip
+RUN pip3 install flask
 COPY app.py /opt/
+COPY /templates/about.html /opt/templates/
 ENTRYPOINT FLASK_APP=/opt/app.py flask run --host=0.0.0.0 --port=8080
--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+color = os.environ.get('APP_COLOR')
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template('about.html', label="Welcome!", color=color)
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you?'
+    return render_template('about.html', label='I am good, how about you?', color=color)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
